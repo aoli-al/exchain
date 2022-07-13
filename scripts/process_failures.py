4ed706ed0088e239b0d711a38f013c938ef7f5d5
Ao Li <leeleo3x@gmail.com>
2022-07-13 16:38:37 -0700

    update.

--- scripts/process_failures.py
+++ scripts/process_failures.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-from typing import Set, Dict, List
+from typing import Dict, List
 
 def read_exception_data(path: str) -> Dict[str, List[str]]:
     data = {}
+    triggered_exceptions = set()
     with open(os.path.join(path, "caught.txt")) as caught:
         for line in caught:
             [exception, trace] = line.split("thrown at")
             if exception not in data:
                 data[exception] = []
             data[exception].append(trace)
-    return data
+            triggered_exceptions.add(line)
+    return data, triggered_exceptions
 
 if __name__=="__main__":
-    normal = read_exception_data("data/no_injection")
-    #  for i in range(20):
-    i = 0
-    case = read_exception_data(f"data/injection_{i}")
-    diff = case.keys() - normal.keys()
-    print(diff)
-
+    normal, normal_exceptions = read_exception_data("data/no_injection")
+    for i in range(4, 5):
+        case, case_exceptions = read_exception_data(f"data/injection_{i}")
+        diff = case.keys() - normal.keys()
+        print(diff)
+        for exception in case_exceptions - normal_exceptions:
+            print(exception.split("thrown at")[0])
