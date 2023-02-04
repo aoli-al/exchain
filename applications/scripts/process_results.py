6f7ea6c8d336fd8ef28eccc91080d9667b94275b
Ao Li <leeleo3x@gmail.com>
2023-02-04 15:48:49 -0500

    update.

--- applications/scripts/process_results.py
+++ applications/scripts/process_results.py
@@ -1,5 +1,5 @@
 from runner import BENCHMARK_APPLICATIONS
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Set
 import json
 import os
 from commons import *
@@ -12,23 +12,29 @@ def read_exceptions(path: str) -> Dict[int, Dict]:
             result[data['label']] = data
     return result
 
-def read_dependencies(path: str) -> List[Tuple[int, int]]:
-    result = []
+def read_dependencies(path: str) -> Set[Tuple[int, int]]:
+    result = set()
     with open(path) as f:
         for line in f:
             cause, exc = [int(x) for x in line.split(",")]
-            print(cause, exc)
+            result.add((cause, exc))
+    return result
 
 def analyze_dynamic_results():
     for name, cls in BENCHMARK_APPLICATIONS.items():
-        print(f"Start processing {name}")
+        print(f"\n\n=================== Start processing {name}")
         app = cls()
         type = "dynamic"
         base_dir = os.path.join(app.out_path, f"{type}-results")
         latest = open(os.path.join(base_dir, "latest")).read().strip()
         work_dir = os.path.join(base_dir, latest)
         exception_data = read_exceptions(os.path.join(work_dir, "exception.json"))
-        read_dependencies(os.path.join(work_dir, "dynamic_dependency.json"))
+        dependencies = read_dependencies(os.path.join(work_dir, "dynamic_dependency.json"))
+        for (cause, exec) in dependencies:
+            e1 = exception_data[cause]
+            e2 = exception_data[exec]
+            print(f"{e1['type']}:{e1['message']}")
+            print(f"{e2['type']}:{e2['message']}")
 
 
 
