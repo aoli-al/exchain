a8cfaf3d62fe88fe5f744a2d2070a53fa9224a3f
Ao Li <leeleo3x@gmail.com>
2023-03-08 15:47:34 -0500

    update.

--- applications/wicket_bench/src/main/java/org/apache/wicket/myapplication/HomePage3.java
+++ applications/wicket_bench/src/main/java/org/apache/wicket/myapplication/HomePage3.java
@@ -23,6 +23,9 @@ import java.util.Random;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.image.InlineImage;
+import org.apache.wicket.markup.html.image.Picture;
+import org.apache.wicket.markup.html.image.Source;
 import org.apache.wicket.markup.html.image.resource.BufferedDynamicImageResource;
 import org.apache.wicket.markup.html.image.resource.DefaultButtonImageResource;
 import org.apache.wicket.markup.html.image.resource.RenderedDynamicImageResource;
@@ -30,6 +33,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.request.resource.IResource;
 import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.request.resource.ResourceReference;
+import org.apache.wicket.request.resource.SharedResourceReference;
 
 
 /**
@@ -54,6 +58,9 @@ public final class HomePage3 extends WebPage
         }
     }
 
+    private static final ResourceReference RESOURCE_REF = new PackageResourceReference(HomePage3.class,
+            "Image2.gif");
+
     /**
      * Constructor
      */
@@ -62,16 +69,59 @@ public final class HomePage3 extends WebPage
         // Image as package resource
         add(new Image("image2", new PackageResourceReference(HomePage3.class, "Image2.gif")));
 
-//        // Dynamically created image. Will re-render whenever resource is asked
-//        // for.
+        // Dynamically created image. Will re-render whenever resource is asked
+        // for.
         add(new Image("image3", new CircleDynamicImageResource(100, 100)));
-//
-//        // Simple model
-//        add(new Image("image4", new Model<>("Image2.gif")));
-//
-//        // Dynamically created buffered image
+
+        // Simple model
+        add(new Image("image4", new Model<>("Image2.gif")));
+
+        // Dynamically created buffered image
 //        add(new Image("image5", getImage5Resource()));
 
+        // Add okay button image
+        add(new Image("okButton", getOkButtonImage()));
+
+        // Add cancel button image
+
+        // image loaded as resource ref via model.
+        add(new Image("imageModelResourceReference", new Model<>(RESOURCE_REF)));
+
+        // image loaded as resource via model.
+        add(new Image("imageModelResource", new Model<>(new CircleDynamicImageResource(100, 100))));
+
+        // responsive images (only for img tag)
+        // the first package resource reference is used for the src attribute all following for the
+        // srcset in the order they are given to the constructor
+        Image respImage = new Image("image6", new PackageResourceReference(this.getClass(),
+                "Image2_small.gif"), new PackageResourceReference(this.getClass(), "Image2_small.gif"),
+                new PackageResourceReference(this.getClass(), "Image2_medium.gif"),
+                new PackageResourceReference(this.getClass(), "Image2_large.gif"));
+        // the x values are applied after each given package resource reference in the order they
+        // are applied to the setter in the srcset attribute
+        respImage.setXValues("320w", "2x", "900w");
+        // The sizes are applied comma separated to the sizes attribute
+        respImage.setSizes("(min-width: 50em) 33vw", "(min-width: 28em) 50vw", "100vw");
+        this.add(respImage);
+
+        // responsive images (to demonstrate the same picture is used for sources and img)
+        Picture picture = new Picture("picture");
+        Source large = new Source("sourcelarge", new PackageResourceReference(this.getClass(),
+                "Image2_large.gif"));
+        large.setMedia("(min-width: 650px)");
+        large.setSizes("(min-width: 50em) 33vw");
+        picture.add(large);
+        large.setOutputMarkupId(true);
+        Source medium = new Source("sourcemedium", new PackageResourceReference(this.getClass(),
+                "Image2_medium.gif"));
+        medium.setMedia("(min-width: 465px)");
+        picture.add(medium);
+        Image image3 = new Image("image7", new PackageResourceReference(this.getClass(),
+                "Image2_small.gif"));
+        picture.add(image3);
+        this.add(picture);
+
+        add(new InlineImage("inline", new PackageResourceReference(getClass(),"Image2.gif")));
     }
 
     /**
