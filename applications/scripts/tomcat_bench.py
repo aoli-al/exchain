f617612419970a850a6253cba0219c32b6c43c2e
Ao Li <leeleo3x@gmail.com>
2023-03-10 12:29:49 -0500

    update.

--- applications/scripts/tomcat_bench.py
+++ applications/scripts/tomcat_bench.py
@@ -30,8 +30,8 @@ class Tomcat(WrappedTest):
     def post(self, type: str, debug: bool, cmd: subprocess.Popen, iter: int, disable_cache: bool):
         time.sleep(10)
         subprocess.call(
-            "ab -c 100 -n 10000 http://localhost:8080/", shell=True)
-        measure = subprocess.Popen("ab -c 100 -n 10000 http://localhost:8080/", shell=True,
+            "ab -c 200 -n 10000 http://localhost:8080/", shell=True)
+        measure = subprocess.Popen("ab -c 200 -n 10000 http://localhost:8080/", shell=True,
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = measure.communicate()
         print(out.decode("utf-8"))
