45ccd7379da6cc0fd75bfce8ecd75258b161b1b5
Ao Li <leeleo3x@gmail.com>
2023-08-30 15:37:42 -0400

    update.

--- instrumentation/src/main/kotlin/al/aoli/exchain/instrumentation/Main.kt
+++ instrumentation/src/main/kotlin/al/aoli/exchain/instrumentation/Main.kt
@@ -4,62 +4,62 @@ import al.aoli.exchain.runtime.NativeRuntime
 import java.lang.instrument.Instrumentation
 
 fun premain(arguments: String?, instrumentation: Instrumentation) {
-    // We cannot enable bytebuddy for now because
-    // we have to instrument bytebuddy which is slow...
-    // If we leave bytebuddy uninstrumented, we will see
-    // crashes when bytebuddy gets a multi-dim array
-    // from an instrumented class.
-    //    AgentBuilder.Default()
-    //        .with(AgentBuilder.RedefinitionStrategy.REDEFINITION)
-    //        .with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
-    //        .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
-    //        .with(object: AgentBuilder.Listener.Adapter() {
-    //            override fun onTransformation(
-    //                typeDescription: TypeDescription,
-    //                classLoader: ClassLoader?,
-    //                module: JavaModule?,
-    //                loaded: Boolean,
-    //                dynamicType: DynamicType
-    //            ) {
-    //                super.onTransformation(typeDescription, classLoader, module, loaded,
-    // dynamicType)
-    //                TransformedCodeStore.store[dynamicType.typeDescription.typeName] =
-    // dynamicType.bytes
-    //            }
-    //        })
-    // //        .with(AgentBuilder.Listener.StreamWriting.toSystemOut().withTransformationsOnly())
-    //        .disableClassFormatChanges()
-    //        .type(not(
-    //            nameStartsWith<TypeDescription>("java")
-    //                .or(nameStartsWith("jdk"))
-    //                .or(nameStartsWith("java"))
-    //                .or(nameStartsWith("javax"))
-    //                .or(nameStartsWith("sun"))
-    //                .or(nameStartsWith("ch.qos"))
-    //                .or(nameStartsWith("com.sun"))
-    //                .or(nameStartsWith("net.bytebuddy"))
-    //                .or(nameStartsWith("org.objectweb"))
-    //                .or(nameStartsWith("shadow.asm"))
-    //                .or(nameStartsWith("kotlin"))
-    //                .or(nameStartsWith("al.aoli.exception.instrumentation"))
-    //                .or(nameStartsWith("org.quartz"))
-    //                .or(nameStartsWith("com.intellij"))
-    //                .or(nameStartsWith("edu.columbia.cs.psl"))
-    //                .or(nameContains("FastClassBySpringCGLIB"))
-    //                .or(nameContains("\$Proxy")))
-    //        )
-    //        .transform { builder, type, _, _ ->
-    //            builder
-    // //            builder
-    // ////                .visit(Advice.to(ExceptionAdvices::class.java).on(isMethod()))
-    // //                .visit(
-    // //                    AsmVisitorWrapper.ForDeclaredMethods()
-    // ////                        .writerFlags(ClassWriter.COMPUTE_MAXS or
-    // ClassWriter.COMPUTE_FRAMES)
-    // ////                        .readerFlags(ClassReader.EXPAND_FRAMES)
-    // //                        .invokable(any(), TryCatchBlockTransformer(type.typeName))
-    // //                )
-    //        }
-    //        .installOn(instrumentation)
-    NativeRuntime.initializedCallback()
+  // We cannot enable bytebuddy for now because
+  // we have to instrument bytebuddy which is slow...
+  // If we leave bytebuddy uninstrumented, we will see
+  // crashes when bytebuddy gets a multi-dim array
+  // from an instrumented class.
+  //    AgentBuilder.Default()
+  //        .with(AgentBuilder.RedefinitionStrategy.REDEFINITION)
+  //        .with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
+  //        .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
+  //        .with(object: AgentBuilder.Listener.Adapter() {
+  //            override fun onTransformation(
+  //                typeDescription: TypeDescription,
+  //                classLoader: ClassLoader?,
+  //                module: JavaModule?,
+  //                loaded: Boolean,
+  //                dynamicType: DynamicType
+  //            ) {
+  //                super.onTransformation(typeDescription, classLoader, module, loaded,
+  // dynamicType)
+  //                TransformedCodeStore.store[dynamicType.typeDescription.typeName] =
+  // dynamicType.bytes
+  //            }
+  //        })
+  // //        .with(AgentBuilder.Listener.StreamWriting.toSystemOut().withTransformationsOnly())
+  //        .disableClassFormatChanges()
+  //        .type(not(
+  //            nameStartsWith<TypeDescription>("java")
+  //                .or(nameStartsWith("jdk"))
+  //                .or(nameStartsWith("java"))
+  //                .or(nameStartsWith("javax"))
+  //                .or(nameStartsWith("sun"))
+  //                .or(nameStartsWith("ch.qos"))
+  //                .or(nameStartsWith("com.sun"))
+  //                .or(nameStartsWith("net.bytebuddy"))
+  //                .or(nameStartsWith("org.objectweb"))
+  //                .or(nameStartsWith("shadow.asm"))
+  //                .or(nameStartsWith("kotlin"))
+  //                .or(nameStartsWith("al.aoli.exception.instrumentation"))
+  //                .or(nameStartsWith("org.quartz"))
+  //                .or(nameStartsWith("com.intellij"))
+  //                .or(nameStartsWith("edu.columbia.cs.psl"))
+  //                .or(nameContains("FastClassBySpringCGLIB"))
+  //                .or(nameContains("\$Proxy")))
+  //        )
+  //        .transform { builder, type, _, _ ->
+  //            builder
+  // //            builder
+  // ////                .visit(Advice.to(ExceptionAdvices::class.java).on(isMethod()))
+  // //                .visit(
+  // //                    AsmVisitorWrapper.ForDeclaredMethods()
+  // ////                        .writerFlags(ClassWriter.COMPUTE_MAXS or
+  // ClassWriter.COMPUTE_FRAMES)
+  // ////                        .readerFlags(ClassReader.EXPAND_FRAMES)
+  // //                        .invokable(any(), TryCatchBlockTransformer(type.typeName))
+  // //                )
+  //        }
+  //        .installOn(instrumentation)
+  NativeRuntime.initializedCallback()
 }
--- instrumentation/src/main/kotlin/al/aoli/exchain/instrumentation/transformers/CatchBlockTransformer.kt
+++ instrumentation/src/main/kotlin/al/aoli/exchain/instrumentation/transformers/CatchBlockTransformer.kt
@@ -1,6 +1,7 @@
 package al.aoli.exchain.instrumentation.transformers
 
 import al.aoli.exchain.runtime.analyzers.ExceptionFlowAnalyzer
+import java.io.File
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Opcodes.ASM8
@@ -14,7 +15,6 @@ import org.objectweb.asm.tree.MethodInsnNode
 import org.objectweb.asm.tree.MethodNode
 import org.objectweb.asm.tree.TryCatchBlockNode
 import org.objectweb.asm.tree.analysis.BasicInterpreter
-import java.io.File
 
 class CatchBlockTransformer(
     private val owner: String,
@@ -25,184 +25,171 @@ class CatchBlockTransformer(
     signature: String?,
     exceptions: Array<String>?
 ) : MethodNode(ASM8, access, name, descriptor, signature, exceptions) {
-    val modifiedInstructions = InsnList()
+  val modifiedInstructions = InsnList()
 
-    private fun processCatchFrames(
-        tryReachedBlocks: Set<AbstractInsnNode>,
-        handlerReachedBlocks: Set<AbstractInsnNode>,
-        instructionMap: Map<AbstractInsnNode, Set<AbstractInsnNode>>,
-        handler: LabelNode
-    ) {
-        var maxLabelIndex = 0
-        var endInst: AbstractInsnNode? = null
-        for (inst in (handlerReachedBlocks - tryReachedBlocks)) {
-            if (instructions.indexOf(inst) > maxLabelIndex) {
-                maxLabelIndex = instructions.indexOf(inst)
-                endInst = inst
-            }
-            if (instructionMap.getOrDefault(inst, emptySet()).isEmpty()) {
-                if (inst.opcode != Opcodes.ATHROW) {
-                    instructions.insertBefore(
-                        inst,
-                        MethodInsnNode(
-                            Opcodes.INVOKESTATIC,
-                            "al/aoli/exchain/runtime/objects/ExceptionRuntime",
-                            "onCatch",
-                            "()V"
-                        )
-                    )
-                }
+  private fun processCatchFrames(
+      tryReachedBlocks: Set<AbstractInsnNode>,
+      handlerReachedBlocks: Set<AbstractInsnNode>,
+      instructionMap: Map<AbstractInsnNode, Set<AbstractInsnNode>>,
+      handler: LabelNode
+  ) {
+    var maxLabelIndex = 0
+    var endInst: AbstractInsnNode? = null
+    for (inst in (handlerReachedBlocks - tryReachedBlocks)) {
+      if (instructions.indexOf(inst) > maxLabelIndex) {
+        maxLabelIndex = instructions.indexOf(inst)
+        endInst = inst
+      }
+      if (instructionMap.getOrDefault(inst, emptySet()).isEmpty()) {
+        if (inst.opcode != Opcodes.ATHROW) {
+          instructions.insertBefore(
+              inst,
+              MethodInsnNode(
+                  Opcodes.INVOKESTATIC,
+                  "al/aoli/exchain/runtime/objects/ExceptionRuntime",
+                  "onCatch",
+                  "()V"))
+        }
+      } else {
+        for (successor in instructionMap[inst]!!) {
+          if (successor in tryReachedBlocks) {
+            assert(inst is JumpInsnNode)
+            if (inst is JumpInsnNode) {
+              if (successor == inst.label) {
+                val insnList = InsnList()
+                val label = LabelNode()
+                insnList.add(JumpInsnNode(Opcodes.GOTO, inst.next as LabelNode))
+                insnList.add(label)
+                insnList.add(
+                    MethodInsnNode(
+                        Opcodes.INVOKESTATIC,
+                        "al/aoli/exchain/runtime/objects/ExceptionRuntime",
+                        "onCatch",
+                        "()V"))
+                insnList.add(JumpInsnNode(Opcodes.GOTO, inst.label))
+                inst.label = label
+                instructions.insert(inst, insnList)
+              } else {
+                instructions.insert(
+                    inst,
+                    MethodInsnNode(
+                        Opcodes.INVOKESTATIC,
+                        "al/aoli/exchain/runtime/objects/ExceptionRuntime",
+                        "onCatch",
+                        "()V"))
+              }
             } else {
-                for (successor in instructionMap[inst]!!) {
-                    if (successor in tryReachedBlocks) {
-                        assert(inst is JumpInsnNode)
-                        if (inst is JumpInsnNode) {
-                            if (successor == inst.label) {
-                                val insnList = InsnList()
-                                val label = LabelNode()
-                                insnList.add(JumpInsnNode(Opcodes.GOTO, inst.next as LabelNode))
-                                insnList.add(label)
-                                insnList.add(
-                                    MethodInsnNode(
-                                        Opcodes.INVOKESTATIC,
-                                        "al/aoli/exchain/runtime/objects/ExceptionRuntime",
-                                        "onCatch",
-                                        "()V"
-                                    )
-                                )
-                                insnList.add(JumpInsnNode(Opcodes.GOTO, inst.label))
-                                inst.label = label
-                                instructions.insert(inst, insnList)
-                            } else {
-                                instructions.insert(
-                                    inst,
-                                    MethodInsnNode(
-                                        Opcodes.INVOKESTATIC,
-                                        "al/aoli/exchain/runtime/objects/ExceptionRuntime",
-                                        "onCatch",
-                                        "()V"
-                                    )
-                                )
-                            }
-                        } else {
-                            instructions.insert(
-                                inst,
-                                MethodInsnNode(
-                                    Opcodes.INVOKESTATIC,
-                                    "al/aoli/exchain/runtime/objects/ExceptionRuntime",
-                                    "onCatch",
-                                    "()V"
-                                )
-                            )
-                        }
-                    }
-                }
+              instructions.insert(
+                  inst,
+                  MethodInsnNode(
+                      Opcodes.INVOKESTATIC,
+                      "al/aoli/exchain/runtime/objects/ExceptionRuntime",
+                      "onCatch",
+                      "()V"))
             }
+          }
         }
-
-        val callCatch = InsnList()
-        callCatch.add(InsnNode(Opcodes.DUP))
-        callCatch.add(
-            MethodInsnNode(
-                Opcodes.INVOKESTATIC,
-                "al/aoli/exchain/runtime/objects/ExceptionRuntime",
-                "onCatchBegin",
-                "(Ljava/lang/Throwable;)V"
-            )
-        )
-
-        instructions.insert(handler.next.next, callCatch)
-
-        val end = LabelNode(InstrumentationLabel())
-        val skip = LabelNode()
-        val insnList = InsnList()
-        insnList.add(JumpInsnNode(Opcodes.GOTO, skip))
-        insnList.add(end)
-        insnList.add(InsnNode(Opcodes.DUP))
-        insnList.add(LdcInsnNode("$owner:$name"))
-        insnList.add(
-            MethodInsnNode(
-                Opcodes.INVOKESTATIC,
-                "al/aoli/exchain/runtime/objects/ExceptionRuntime",
-                "onCatchWithException",
-                "(Ljava/lang/Throwable;Ljava/lang/String;)V"
-            )
-        )
-        insnList.add(InsnNode(Opcodes.ATHROW))
-
-        insnList.add(skip)
-        instructions.insert(endInst, insnList)
-
-        val tryCatchBlock = TryCatchBlockNode(handler, end, end, "java/lang/Throwable")
-        newTryCatchBlocks.add(tryCatchBlock)
+      }
     }
 
-    private val newTryCatchBlocks = ArrayList<TryCatchBlockNode>()
+    val callCatch = InsnList()
+    callCatch.add(InsnNode(Opcodes.DUP))
+    callCatch.add(
+        MethodInsnNode(
+            Opcodes.INVOKESTATIC,
+            "al/aoli/exchain/runtime/objects/ExceptionRuntime",
+            "onCatchBegin",
+            "(Ljava/lang/Throwable;)V"))
 
-    override fun visitEnd() {
-        super.visitEnd()
+    instructions.insert(handler.next.next, callCatch)
 
-        val functionName = "$owner:$name:$desc"
+    val end = LabelNode(InstrumentationLabel())
+    val skip = LabelNode()
+    val insnList = InsnList()
+    insnList.add(JumpInsnNode(Opcodes.GOTO, skip))
+    insnList.add(end)
+    insnList.add(InsnNode(Opcodes.DUP))
+    insnList.add(LdcInsnNode("$owner:$name"))
+    insnList.add(
+        MethodInsnNode(
+            Opcodes.INVOKESTATIC,
+            "al/aoli/exchain/runtime/objects/ExceptionRuntime",
+            "onCatchWithException",
+            "(Ljava/lang/Throwable;Ljava/lang/String;)V"))
+    insnList.add(InsnNode(Opcodes.ATHROW))
 
-        if (tryCatchBlocks.isNotEmpty()) {
-            val analyzer = ExceptionFlowAnalyzer(instructions, BasicInterpreter())
-            // DON'T RUN analyzer twice! It will modify the successors!
-            analyzer.analyze(owner, this, false)
-            //            val exceptionResult = analyzer.analyze(owner, this, true)
-            //            val affectedVars = mutableSetOf<Int>()
-            //            for (index in normalResult.indices) {
-            //                if (normalResult[index] != null) {
-            //                    for (localIndex in 0 until normalResult[index].locals) {
-            //                        val normalLocal = normalResult[index].getLocal(localIndex)
-            //                        val exceptionLocal = exceptionResult[index].getLocal(localIndex)
-            //                        if (normalLocal != exceptionLocal) {
-            //                            for (insn in normalLocal.insns) {
-            //                                if (insn is VarInsnNode) {
-            //                                    affectedVars.add(insn.`var`)
-            //                                }
-            //                            }
-            //                        }
-            //                    }
-            //                }
-            //            }
-            //
-            //            if (affectedVars.isNotEmpty()) {
-            //                dataFlowOutput.appendText("Method $functionName:
-            // [${affectedVars.joinToString(", ")}]\n")
-            //            } else {
-            //                dataFlowOutput.appendText("Method $functionName: EMPTY\n")
-            //            }
+    insnList.add(skip)
+    instructions.insert(endInst, insnList)
 
-            val processedHandlers = mutableSetOf<LabelNode>()
-            for (tryCatchBlock in this.tryCatchBlocks) {
-                if (tryCatchBlock.handler in processedHandlers) continue
-                if (tryCatchBlock.type == null) continue
-                processedHandlers.add(tryCatchBlock.handler)
-                val tryReachedBlocks = analyzer.reachableBlocks(tryCatchBlock.start)
-                val handlerReachedBlocks = analyzer.reachableBlocks(tryCatchBlock.handler)
-                processCatchFrames(
-                    tryReachedBlocks,
-                    handlerReachedBlocks,
-                    analyzer.instructionSuccessors,
-                    tryCatchBlock.handler
-                )
-            }
+    val tryCatchBlock = TryCatchBlockNode(handler, end, end, "java/lang/Throwable")
+    newTryCatchBlocks.add(tryCatchBlock)
+  }
 
-            tryCatchBlocks?.addAll(newTryCatchBlocks)
-        }
+  private val newTryCatchBlocks = ArrayList<TryCatchBlockNode>()
 
-        accept(mv)
+  override fun visitEnd() {
+    super.visitEnd()
+
+    val functionName = "$owner:$name:$desc"
+
+    if (tryCatchBlocks.isNotEmpty()) {
+      val analyzer = ExceptionFlowAnalyzer(instructions, BasicInterpreter())
+      // DON'T RUN analyzer twice! It will modify the successors!
+      analyzer.analyze(owner, this, false)
+      //            val exceptionResult = analyzer.analyze(owner, this, true)
+      //            val affectedVars = mutableSetOf<Int>()
+      //            for (index in normalResult.indices) {
+      //                if (normalResult[index] != null) {
+      //                    for (localIndex in 0 until normalResult[index].locals) {
+      //                        val normalLocal = normalResult[index].getLocal(localIndex)
+      //                        val exceptionLocal = exceptionResult[index].getLocal(localIndex)
+      //                        if (normalLocal != exceptionLocal) {
+      //                            for (insn in normalLocal.insns) {
+      //                                if (insn is VarInsnNode) {
+      //                                    affectedVars.add(insn.`var`)
+      //                                }
+      //                            }
+      //                        }
+      //                    }
+      //                }
+      //            }
+      //
+      //            if (affectedVars.isNotEmpty()) {
+      //                dataFlowOutput.appendText("Method $functionName:
+      // [${affectedVars.joinToString(", ")}]\n")
+      //            } else {
+      //                dataFlowOutput.appendText("Method $functionName: EMPTY\n")
+      //            }
+
+      val processedHandlers = mutableSetOf<LabelNode>()
+      for (tryCatchBlock in this.tryCatchBlocks) {
+        if (tryCatchBlock.handler in processedHandlers) continue
+        if (tryCatchBlock.type == null) continue
+        processedHandlers.add(tryCatchBlock.handler)
+        val tryReachedBlocks = analyzer.reachableBlocks(tryCatchBlock.start)
+        val handlerReachedBlocks = analyzer.reachableBlocks(tryCatchBlock.handler)
+        processCatchFrames(
+            tryReachedBlocks,
+            handlerReachedBlocks,
+            analyzer.instructionSuccessors,
+            tryCatchBlock.handler)
+      }
+
+      tryCatchBlocks?.addAll(newTryCatchBlocks)
     }
 
-    companion object {
-        var varIndex = 0
-        val dataFlowOutput = File("/tmp/data-flow.txt")
-        fun newLocalVar(): String {
-            return "LOCAL_VAR_${varIndex++}"
-        }
+    accept(mv)
+  }
 
-        init {
-            dataFlowOutput.writeText("")
-        }
+  companion object {
+    var varIndex = 0
+    val dataFlowOutput = File("/tmp/data-flow.txt")
+    fun newLocalVar(): String {
+      return "LOCAL_VAR_${varIndex++}"
     }
+
+    init {
+      dataFlowOutput.writeText("")
+    }
+  }
 }
